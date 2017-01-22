c512f8955e9857c2886628ac2d1fc0cd89570f4d
Rajat Rasal <yugiohrajat1@gmail.com>
2017-01-22 13:08:35 +0000

    Added an index.html file for homepage which extends the base.html, added all basic input forms for the start scoring section of the homepage and gave them each a view function

--- searching/forms.py
+++ searching/forms.py
@@ -9,3 +9,25 @@ class TeamnameSearchForm(forms.Form):
             max_length=160,
             label=False 
             )
+
+class MatchDetailsForm(forms.Form):
+    """
+    """
+    
+    #WEATHERCHOICES = ['sunny', 'sunny spells', 'cloudy', 'overcast', 'showers', 'heavy rain', 'rain and sun', 'other']
+    ground = forms.CharField(label="Ground Location")
+#    weather = forms.CharField(label="Weather")
+#    umpire1 = forms.CharField(label="Home Umpire") 
+#    umpire2 = forms.CharField(label="Away Umpire") 
+#    matchformat = forms.IntegerField(label="Match Format", min_value="5", max_value="60") 
+
+class MatchDetailsTest(forms.Form):
+    """
+    """
+    
+    #WEATHERCHOICES = ['sunny', 'sunny spells', 'cloudy', 'overcast', 'showers', 'heavy rain', 'rain and sun', 'other']
+    #weather = forms.ChoiceField(choices=WEATHERCHOICES)
+    #weather = forms.CharField(label="Weather")
+    CHOICES = (('1', 'First',), ('2', 'Second',))
+    choice_field = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+
--- searching/views.py
+++ searching/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.views.generic import View
-from django.http import HttpResponse
 
-from searching.forms import TeamnameSearchForm
+from searching.forms import TeamnameSearchForm, MatchDetailsForm, MatchDetailsTest
 
 class Base(View):
-    
-    def get(self, request):
-        return render(request, 'base.html')
+
+	def get(self, request):
+		return render(request, 'base.html')
 
 class Index(View):
 
-    def get(self, request):
-        form = TeamnameSearchForm()
-        return render(request, 'index.html', {'form': form})
+	def get(self, request):
+		TeamnameSearch = TeamnameSearchForm()
+		MatchDetails = MatchDetailsForm()
+		MatchTest = MatchDetailsTest()
+		
+		return render(request, 'index.html', 
+			{'TeamnameSearchForm': TeamnameSearch,
+			'MatchDetailsForm': MatchDetails,
+			'MatchDetailsTest': MatchTest})
+
+
+if __name__ == "__main__":
+	print('hi')
+	print(TeamnameSearchForm)
+	print(MatchDetailsTest)
 
