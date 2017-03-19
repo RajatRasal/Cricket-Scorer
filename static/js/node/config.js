04d22c409ba9ee2a797a5aa43a8177a5c1cfd5e3
Rajat Rasal <yugiohrajat1@gmail.com>
2017-03-19 19:50:46 +0000

    Created Node app which has interface with Twitter and can post cricket scores to Twitter at regular time intervals

---
+++ static/js/node/config.js
@@ -0,0 +1,15 @@
+// I can put my twitter API keys in here to protect 
+// them from being visible directly through the 
+// source code of the webpage when the page is
+// rendered. This file will not be added to open source
+// repositories, so no one can steal my API keys 
+// if I post this file in the open source, which 
+// could have happened if I left the API keys 
+// hardcoded in the post.js file.
+console.log('here');
+module.exports = {
+	consumer_key:'2lPt9qHDEb3TOKwT5Fpu637jK',
+	consumer_secret:'SZFT0GqKaWulya8Xn7DTrhBdIQw8ppTFMSrUK3AkLcTflZvQ8W',
+	access_token:'1422449718-JegXUIRyZnUg8PcdlXIkK5AHrg1Q9nwAVazP4qq',
+	access_token_secret: 'rhENKA73ybqyhKStMVygDvjwM53B1yVVo2IcXzIdEoWjE'
+}
---
+++ static/js/node/post.js
@@ -0,0 +1,56 @@
+// Link with all my Twitter API Keys  
+// https://apps.twitter.com/app/13556714/keys
+
+// Acts as a import statement to import the 'twit' 
+// library specified as an argument to the require 
+// function. This 'twit' library is found in the 
+// 'node_modules' folder inside this directory 
+// where the require function will default to 
+// looking inside.
+var Twit = require('twit');
+
+// I have put my Twitter API keys in another file to 
+// keep them separate from this main file. This should 
+// make them more secure when rendered in the page 
+// or when put through source control becuase it can 
+// will not add the config file to my open source 
+// repositories.
+var config = require('./config');
+
+// T is a twit object, where the twit library handles 
+// the interface/socket connection between my client
+// side JS and the Twitter page whose API keys I have
+// stored in the config.js file.
+var T = new Twit(config);
+
+// I will create a key-value pair list which will be 
+// treated as a JSON object by the twit object. The 
+// keys in the key-value pair list are all standard
+// keys which are recognised by Twitter in different 
+// ways. 
+// e.g the 'status' key stores the data to be posted 
+post_score();
+
+setInterval(post_score, 1000*20);
+
+function post_score() {
+	var r = Math.floor(Math.random()*100);
+	var tweet = {
+		status: 'HERE IS A RANDOM NUMBER '.concat(r),
+	};
+	T.post('statuses/update', tweet, after_post);
+	
+	function after_post(err, data, response){
+		if (err) {
+			// REMEMEBER that Twitter does not like 
+			// the same status being posted twice 
+			// so this will more often than not be 
+			// the cause of an error!
+			console.log("SOMETHING WENT WRONG");
+			console.log(err);
+		}
+		else {
+			console.log("IT WORKED!");
+		}
+	}
+}
